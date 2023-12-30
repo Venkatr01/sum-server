2d6ba4082e48bd1be8d807fc286e24adc3a5a5fc
Venkat <vraju4865@gmail.com>
2023-12-30 22:26:35 +0530

    trial

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/sum",(req,res)=>{
+    let a = parseInt(req.query.a);
+    let b = parseInt(req.query.b);
+    const sum = a + b;
+    res.send(sum.toString());
+});
+
